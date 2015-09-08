56cb3e4f17b2635bd6de8bb26ed02461ed47cfd3
mjkingdo <mjkingdo@ncsu.edu>
2015-09-08 19:13:16 -0400

    bleh

---
+++ Assets/Pillow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pillow : MonoBehaviour {
+	public GameObject pillow;
+	public Material myMaterial;
+
+	void Start()
+	{
+		Texture[] grillSprites = Resources.LoadAll<Texture>("GrillSprites");
+		myMaterial.SetTexture ("_MainTex", grillSprites [Random.Range (0, grillSprites.Length - 1)]);
+	}
+}
--- Assets/Scripts/spawn.cs
+++ Assets/Scripts/spawn.cs
@@ -2,18 +2,14 @@
 using System.Collections;
 
 public class spawn : MonoBehaviour {
-	public GameObject pillow;
+	public Pillow pillow;
 	public Vector3 startingPosPillow;
 	// Use this for initialization
 	void Start () {
-		startingPosPillow = pillow.transform.position;
-		InvokeRepeating ("Spawn", 1, Random.Range((float) 0.01, (float) (.01)));
+		InvokeRepeating ("Spawn", 1, Random.Range((float) 1, (float) 3));
 	}
 
 	void Spawn(){
-		//Sprite[] grillSprites = Resources.LoadAll<Sprite>("GrillSprites");
-		//SpriteRenderer renderer = pillow.AddComponent<SpriteRenderer>();
-		//renderer.sprite = grillSprites[Random.Range(0, grillSprites.Length - 1)];
 		Vector3 position = new Vector3 (Random.Range(startingPosPillow.x - 5, startingPosPillow.x + 5),
 		                                startingPosPillow.y,
 		                                Random.Range(startingPosPillow.z - 5, startingPosPillow.z + 5));
